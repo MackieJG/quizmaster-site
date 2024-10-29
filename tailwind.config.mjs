52f199d89b6eafe2069c501d4e921370012c89aa
unknown <joshuamackie10@googlemail.com>
2024-10-29 12:50:54 +0000

    created the header banner

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      brown: "#5F432C",
+      beige: "#CBC7B7",
+    },
+    fontFamily: {
+      afacad: ["Afacad Flux", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
